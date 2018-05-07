f89bca8fc856922690139f32b37c6226f887f133
revireximf <36724276+revireximf@users.noreply.github.com>
2018-05-07 12:49:56 +0700

    Update random.js

--- random.js
+++ random.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2018/04/how-to-use-paypal-dollar.html"}
 if(x=="2"){xxx="/2018/04/make-money-online.html"}
 if(x=="3"){xxx="/2018/04/computer-software-and-hardware.html"}
 if(x=="4"){xxx="/2018/04/fiber-optic-cable.html"}
-if(x=="5"){xxx="/2018/04/crusher-stune.html"}a_to_ve[i].href="http://www.machineprinting3d.club/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2018/04/crusher-stune.html"}a_to_ve[i].href="http://www.machineprinting3d.club"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
